ae41053b136b78f8545fbebf58a23e62085326c5
Mike-Schvedov <MikeSchvedovMS@Gmail.com>
2026-04-30 13:04:06 +0300

    deleted security block

--- main.tf
+++ main.tf
@@ -3,12 +3,4 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket   = each.value.bucket_name
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  for_each = var.buckets
-  bucket   = aws_s3_bucket.my_bucket[each.key].id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
