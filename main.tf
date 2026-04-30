627e06c69d30c60bea3e5bcd539e4208735579af
Mike-Schvedov <MikeSchvedovMS@Gmail.com>
2026-04-30 10:51:45 +0300

    force trigger fail

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "cortex-repro-bucket-2026"
 }
 
-
+# This is the exact code that triggers the "READ permission to everyone" rule
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.test_bucket.id
+  acl    = "public-read"
+}
