2d98275bdf2cbbd3ad74d88dc59baa9374358c65
Mike-Schvedov <MikeSchvedovMS@Gmail.com>
2026-04-30 10:34:02 +0300

    add insecure bucket

--- main.tf
+++ main.tf
@@ -2,10 +2,4 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "cortex-repro-bucket-2026"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.test_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+
